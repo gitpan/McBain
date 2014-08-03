834dae80467dbda4fbdc740fa1ae06c8915190ae
עידו פרלמוטר (Ido Perlmuter) <ido@ido50.net>
2014-08-03 17:30:20 +0000

    Import of IDOPEREL/McBain-1.003000 from CPAN.
    gitpan-cpan-distribution: McBain
    gitpan-cpan-version:      1.003000
    gitpan-cpan-path:         IDOPEREL/McBain-1.003000.tar.gz
    gitpan-cpan-author:       IDOPEREL
    gitpan-cpan-maturity:     released

--- t/lib/Mendoza.pm
+++ t/lib/Mendoza.pm
@@ -14,6 +14,24 @@ get '/status' => (
 	cb => sub { shift->status }
 );
 
+get '/(pre|post)_route_test' => (
+	cb => sub { 'asdf' }
+);
+
+pre_route {
+	my ($api, $ns, $params) = @_;
+
+	croak { code => 500, error => "pre_route doesn't like you" }
+		if $ns eq 'GET:/pre_route_test';
+};
+
+post_route {
+	my ($api, $ns, $result) = @_;
+
+	$$result = 'post_route messed you up'
+		if $ns eq 'GET:/post_route_test';
+};
+
 sub new { bless { status => 'ALL IS WELL' }, shift };
 
 sub status { shift->{status} }
